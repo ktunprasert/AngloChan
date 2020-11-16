8612c83e76a31b9a25644bfe49775f1de1f45f65
Kris <ktunprasert@outlook.com>
2020-11-16 22:15:31 +0000

    hotfix server path config

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -52,7 +52,7 @@ class PostsController extends Controller {
                 if (config("app.env") === "local") {
                     putenv('PATH="C:\Program Files\ImageMagick-7.0.8-Q16\"');
                 } else {
-                    putenv('PATH="/usr/local/bin/"');
+                    putenv('PATH=/usr/local/bin');
                 }
                 $out = shell_exec("ffmpeg -y -i {$tmp} -vframes 1 -filter:v scale='-1:300' \"thumbnails/" . $thumbnail . "\" 2>&1");
                 $post->out = $out;
